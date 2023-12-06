b0f2ac2b44d7f709f2277bee31865451a732450e
Elayade Ismail <elayadeismail@gmail.com>
2023-12-06 21:37:29 +0100

    Add check to ENV vars

---
+++ src/utils/env.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+const EnvSchema = z.object({
+	NODE_ENV: z.enum(['production', 'development', 'test'] as const),
+	DATABASE_URL: z.string(),
+});
+
+const result = EnvSchema.safeParse(process.env);
+if (!result.success) {
+	console.log('Missing Env vars', result.error.formErrors.fieldErrors);
+	process.exit(1);
+}
+
+export const Env = result.data;
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv extends z.infer<typeof EnvSchema> {}
+	}
+}

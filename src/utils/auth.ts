fe262ff570a24c08620fd5adcfe9d4d5dc681624
Elayade Ismail <elayadeismail@gmail.com>
2023-12-07 17:04:25 +0100

    Add user Authenticated Layout

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,18 +1,31 @@
 'use server';
 import jwt from 'jsonwebtoken';
 import { cookies } from 'next/headers';
+import { Env } from './env';
+
+const AUTH_COOKIE_NAME = 'auth';
 
 export const setAuthCookie = ({ userId }: { userId: string }) => {
-	const signedToken = jwt.sign({ userId }, process.env.COOKIE_SECRET ?? '', {
+	const signedToken = jwt.sign({ userId }, Env.COOKIE_SECRET, {
 		expiresIn: '7d',
 	});
 
 	cookies().set({
-		name: 'auth',
+		name: AUTH_COOKIE_NAME,
 		value: signedToken,
 		httpOnly: true,
 		secure: process.env.NODE_ENV === 'production',
 		sameSite: 'lax',
 		path: '/',
 	});
+};
+
+export const isAuthenticated = (): boolean => {
+	const token = cookies().get(AUTH_COOKIE_NAME)?.value ?? '';
+	try {
+		jwt.verify(token, Env.COOKIE_SECRET);
+		return true;
+	} catch (error) {
+		return false;
+	}
 };

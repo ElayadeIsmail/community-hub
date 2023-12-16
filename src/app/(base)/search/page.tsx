468c89cad8450e4240c4c1ac4555a61f9e5088e7
Elayade Ismail <elayadeismail@gmail.com>
2023-12-16 16:08:08 +0100

    Add Search Page

--- src/app/(base)/search/page.tsx
+++ src/app/(base)/search/page.tsx
@@ -1,4 +1,10 @@
+import { getSearchPosts } from '@/db/queries/posts';
 import { redirect } from 'next/navigation';
+import { Suspense } from 'react';
+import {
+	PostList,
+	PostListSkeleton,
+} from '../community/[slug]/posts/components';
 
 interface SearchPageProps {
 	searchParams: {
@@ -10,7 +16,23 @@ const SearchPage = ({ searchParams }: SearchPageProps) => {
 	if (!searchParams.term) {
 		redirect('/');
 	}
-	return <div>page</div>;
+	return (
+		<section className='container grid grid-cols-4 gap-4 p-4'>
+			<div className='col-span-3 '>
+				<h1 className='text-4xl font-bold my-4'>
+					Search Result For &quot;{searchParams.term}&quot;
+				</h1>
+				<Suspense fallback={<PostListSkeleton />}>
+					<PostList
+						fetcher={async (cursor?: string) => {
+							'use server';
+							return getSearchPosts(searchParams.term, cursor);
+						}}
+					/>
+				</Suspense>
+			</div>
+		</section>
+	);
 };
 
 export default SearchPage;
--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -100,3 +100,65 @@ export const getPostsBySlug = async (slug: string, cursor?: string) => {
 	});
 	return posts;
 };
+export const getSearchPosts = async (searchTerm: string, cursor?: string) => {
+	const session = await auth();
+
+	const posts = await db.post.findMany({
+		where: {
+			OR: [
+				{
+					community: {
+						slug: { contains: searchTerm },
+					},
+				},
+				{
+					title: { contains: searchTerm },
+				},
+				{
+					content: {
+						contains: searchTerm,
+					},
+				},
+			],
+		},
+		select: {
+			id: true,
+			createdAt: true,
+			title: true,
+			points: true,
+			community: {
+				select: {
+					slug: true,
+				},
+			},
+			author: {
+				select: {
+					name: true,
+					image: true,
+				},
+			},
+			_count: {
+				select: {
+					comments: true,
+				},
+			},
+			votes: session
+				? {
+						where: {
+							userId: session.user.id,
+						},
+						select: {
+							value: true,
+						},
+				  }
+				: undefined,
+		},
+		cursor: typeof cursor === 'string' ? { id: cursor } : undefined,
+		skip: typeof cursor === 'string' ? 1 : 0,
+		take: 20,
+		orderBy: {
+			points: 'desc',
+		},
+	});
+	return posts;
+};
